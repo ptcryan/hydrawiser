ec03031e5ab35653e482cd2a4741be2a940d912c
pi <ptcryan@gmail.com>
2018-04-17 08:00:47 -0400

    Updated license description.

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
         'Natural Language :: English',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
+    ],
 )
