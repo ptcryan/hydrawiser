a014ffef8db0a4ce67cf4bb6fc206c2043e937aa
ptcryan <ptcryan@gmail.com>
2020-02-23 23:07:36 -0500

    Updates for API v1.4

--- hydrawiser/__init__.py
+++ hydrawiser/__init__.py
@@ -1,11 +1,11 @@
 """
-Copyright (c) 2018 David Ryan
+Copyright (c) 2020 David Ryan
 
 Python library for interfacting with Hunter Industries, Inc. Hydrawise HC
 and Pro-HC sprinkler controllers.
 """
 
 __author__ = 'David Ryan'
-__version__ = '0.1.1'
-__copyright__ = 'Copyright (c) 2018 David Ryan'
+__version__ = '0.2'
+__copyright__ = 'Copyright (c) 2020 David Ryan'
 __license__ = 'MIT'
